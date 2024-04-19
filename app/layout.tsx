73286fe9480b7824a01b72bf107bd7539445a747
Ty Mason <hi@tygr.dev>
2024-04-19 20:45:00 +0100

    feat: add awards, footer and redirects

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme/provider";
 import Navbar from "@/components/navbar";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 const literata = Literata({ subsets: ["latin"], variable: "--font-serif" });
@@ -38,6 +39,19 @@ export default function RootLayout({
                         <div className="flex flex-col max-w-[1000px] items-center w-full">
                             <Navbar />
                             {children}
+                            <footer className="py-2">
+                                <p className="text-neutral-500 dark:text-neutral-400">
+                                    © {new Date().getFullYear()} Casey Dyke. All
+                                    rights reserved. Made with ❤️ by{" "}
+                                    <Link
+                                        href="https://tygr.dev"
+                                        className="text-blue-500"
+                                        target="_blank"
+                                    >
+                                        tygrdev
+                                    </Link>
+                                </p>
+                            </footer>
                         </div>
                     </main>
                 </ThemeProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { ThemeToggle } from "@/components/theme/toggle";
 import { Button } from "@/components/ui/button";
 import { cms } from "@/lib/directus";
 import Image from "next/image";
 import Link from "next/link";
 import { readItems } from "@directus/sdk";
 import Gallery from "@/components/gallery";
+import { Crown, Medal } from "@/components/icons";
 
 export default async function Home() {
     const result = await cms.request(readItems("photos"));
@@ -56,22 +56,97 @@ export default async function Home() {
                     </p>
                 </div>
                 <p className="text-neutral-800 dark:text-neutral-200">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Fusce condimentum tristique ante. Proin congue, odio a
-                    ullamcorper congue, erat augue pretium felis, nec dapibus
-                    libero turpis vel metus. Quisque a dui tristique, porttitor
-                    nibh vitae, aliquam lorem. In id ornare purus. Sed congue et
-                    ante in fermentum. Vivamus commodo arcu in urna porta, sit
-                    amet mollis tortor luctus. Curabitur in velit mi. In vitae
-                    turpis ipsum. Donec non turpis risus. Cras porta commodo
-                    massa ullamcorper euismod. <br /> <br />
-                    In ullamcorper quam vitae est cursus mollis. Duis ut rhoncus
-                    nisi, non tincidunt ante. Integer a mattis odio. Duis vel
-                    lorem vel odio sagittis scelerisque a nec enim. Aliquam et
-                    dignissim libero. Etiam eleifend lacinia felis, eget
-                    venenatis lorem tempus finibus. Nam quis elit ipsum. Sed
-                    vitae venenatis metus. Curabitur eleifend sit amet leo a
-                    consectetur. <br /> <br />
+                    At a young age, I was infatuated with animals and wildlife.
+                    As I got older, I started studying animal and zoo
+                    management. As my passion grew, I wanted to find a way for
+                    others to appreciate animals the same way I do. After
+                    discovering animal photographers and their impressive work,
+                    I took up the idea of becoming one myself in hopes of
+                    sharing my passion with the world. <br /> <br /> In 2021, I
+                    purchased{" "}
+                    <a
+                        href="https://www.nikon.co.uk/en_GB/product/cameras/d780"
+                        className="text-accent-700"
+                        target="_blank"
+                    >
+                        my first DSLR camera
+                    </a>{" "}
+                    and used my own animals as subjects to practice on. I&apos;d
+                    also take regular trips to{" "}
+                    <a
+                        href="https://www.facebook.com/sotonreptile"
+                        className="text-accent-700"
+                        target="_blank"
+                    >
+                        Southampton Reptile Centre
+                    </a>{" "}
+                    to photograph their animals as well. Soon enough, they took
+                    notice and an agreement was established that they could use
+                    my photos for their marketing, and I would recieve service
+                    with no charge. <br /> <br /> After building some
+                    confidence, I took the deep dive and started photographing
+                    animals at major zoos, such as{" "}
+                    <a
+                        href="https://www.marwell.org.uk/"
+                        className="text-accent-700"
+                        target="_blank"
+                    >
+                        Marwell Wildlife
+                    </a>
+                    ,{" "}
+                    <a
+                        href="https://www.londonzoo.org/"
+                        className="text-accent-700"
+                        target="_blank"
+                    >
+                        ZSL London Zoo
+                    </a>
+                    ,{" "}
+                    <a
+                        href="https://www.exmoorzoo.co.uk/"
+                        className="text-accent-700"
+                        target="_blank"
+                    >
+                        Exmoor Zoo
+                    </a>
+                    , and more. Just over a year later, I entered the Marwell
+                    Wildlife Photography competition and got my picture of their
+                    Two Toed Sloth in{" "}
+                    <a
+                        href="/awards/marwell-2023"
+                        className="text-accent-700"
+                        target="_blank"
+                    >
+                        their 2023 calendar
+                    </a>
+                    . A few months later, I entered the 2022 BIAZA Photo
+                    competition and was awarded with a{" "}
+                    <a
+                        href="https://biaza.org.uk/news/detail/biaza-annual-photo-competition-2022-winners-feature-some-of-the-weirdest-lesser-known-zoo-and-aquarium-animals#:~:text=Runner%20up%20%2D%20Earing%20the%20February%20Blooms%2C%20Casey%20Dyke%20(Marwell%20Wildlife)"
+                        className="text-accent-700"
+                        target="_blank"
+                    >
+                        Runner up accolade
+                    </a>{" "}
+                    for my photo of a Swift Parrot. <br /> <br /> Determined to
+                    get first place, in 2023, I entered the BIAZA Photo
+                    competition again with photos of an{" "}
+                    <a
+                        href="https://biaza.org.uk/news/detail/biaza-photo-competition-2023-the-stunning-winning-photos#:~:text=WINNER%20%2D%20Casey%20Dyke%20%7C%20Marwell%20Wildlife%20%7C%20Marwell%27s%20Emerald"
+                        className="text-accent-700"
+                        target="_blank"
+                    >
+                        emerald tree monitor
+                    </a>{" "}
+                    from Marwell Wildlife, and a{" "}
+                    <a
+                        href="https://biaza.org.uk/news/detail/biaza-photo-competition-2023-the-stunning-winning-photos#:~:text=WINNER%20%2D%20Casey%20Dyke%20%7C%20ZSL%20London%20Zoo%20%7C%C2%A0Jelly%20Tranquillity"
+                        className="text-accent-700"
+                        target="_blank"
+                    >
+                        nettle jellyfish
+                    </a>{" "}
+                    from ZSL London Zoo and won both awards!
                 </p>
             </div>
 
@@ -104,28 +179,78 @@ export default async function Home() {
                         photos={result.filter((x) => x.featured === true)}
                         breakPoints={{ 350: 1, 750: 2, 900: 3 }}
                     />
-                    {/* {result
-                        .filter((x) => x.featured === true)
-                        .map((img) => {
-                            return (
-                                <>
-                                    <Image
-                                        src={
-                                            process.env.NEXT_PUBLIC_CMS_URL +
-                                            "/assets/" +
-                                            img.image
-                                        }
-                                        alt={img.collection}
-                                        width={1080}
-                                        height={1080}
-                                        key={img.id}
-                                        quality={75}
-                                        className="w-[540px] h-[540px] object-cover object-center rounded-lg border border-black/10 dark:border-white/10"
-                                    />
-                                </>
-                            );
-                        })} */}
                 </div>
+            </div>
+
+            <div className="flex flex-col gap-4 items-start w-full">
+                <div className="flex flex-col sm:flex-col items-start gap-0 justify-start w-full">
+                    <h2 className="text-3xl font-bold font-serif">
+                        Awards & Accolades
+                    </h2>
+                    <p className="text-neutral-500 dark:text-neutral-400 text-sm">
+                        A list of awards I&apos;ve won over the years.
+                    </p>
+                </div>
+                <Link
+                    href="/marwell-2023"
+                    className="flex flex-row items-center w-full p-2 overflow-hidden rounded-md whitespace-nowrap text-ellipsis hover:bg-neutral-200 dark:hover:bg-neutral-800"
+                >
+                    <p className="gap-2 font-medium flex flex-row ">
+                        <Crown width={18} />
+                        Winner
+                    </p>
+                    <span className="hidden w-full sm:flex sm:flex-row sm:items-center ">
+                        <hr className="flex-1 mx-2 border border-black/10 dark:border-white/10" />
+                        <span className="text-sm font-medium text-neutral-500">
+                            Marwell Wildlife Photography Competition 2023
+                        </span>
+                    </span>
+                </Link>
+                <Link
+                    href="https://biaza.org.uk/news/detail/biaza-annual-photo-competition-2022-winners-feature-some-of-the-weirdest-lesser-known-zoo-and-aquarium-animals#:~:text=Runner%20up%20%2D%20Earing%20the%20February%20Blooms%2C%20Casey%20Dyke%20(Marwell%20Wildlife)"
+                    className="flex flex-row items-center w-full p-2 overflow-hidden rounded-md whitespace-nowrap text-ellipsis hover:bg-neutral-200 dark:hover:bg-neutral-800"
+                >
+                    <p className="gap-2 font-medium flex flex-row ">
+                        <Medal width={18} />
+                        Runner Up
+                    </p>
+                    <span className="hidden w-full sm:flex sm:flex-row sm:items-center ">
+                        <hr className="flex-1 mx-2 border border-black/10 dark:border-white/10" />
+                        <span className="text-sm font-medium text-neutral-500">
+                            BIAZA Photo Competition 2022
+                        </span>
+                    </span>
+                </Link>
+                <Link
+                    href="https://biaza.org.uk/news/detail/biaza-photo-competition-2023-the-stunning-winning-photos#:~:text=WINNER%20%2D%20Casey%20Dyke%20%7C%20Marwell%20Wildlife%20%7C%20Marwell%27s%20Emerald"
+                    className="flex flex-row items-center w-full p-2 overflow-hidden rounded-md whitespace-nowrap text-ellipsis hover:bg-neutral-200 dark:hover:bg-neutral-800"
+                >
+                    <p className="gap-2 font-medium flex flex-row">
+                        <Crown width={18} />
+                        Reptile Winner
+                    </p>
+                    <span className="hidden w-full sm:flex sm:flex-row sm:items-center">
+                        <hr className="flex-1 mx-2 border w-full border-black/10 dark:border-white/10" />
+                        <span className="text-sm font-medium text-neutral-500">
+                            BIAZA Photo Competition 2023
+                        </span>
+                    </span>
+                </Link>
+                <Link
+                    href="https://biaza.org.uk/news/detail/biaza-photo-competition-2023-the-stunning-winning-photos#:~:text=WINNER%20%2D%20Casey%20Dyke%20%7C%20ZSL%20London%20Zoo%20%7C%C2%A0Jelly%20Tranquillity"
+                    className="flex flex-row items-center w-full p-2 overflow-hidden rounded-md whitespace-nowrap text-ellipsis hover:bg-neutral-200 dark:hover:bg-neutral-800"
+                >
+                    <p className="gap-2 font-medium flex flex-row ">
+                        <Crown width={18} />
+                        Invertebrate Winner
+                    </p>
+                    <span className="hidden w-full sm:flex sm:flex-row sm:items-center ">
+                        <hr className="flex-1 mx-2 border border-black/10 dark:border-white/10" />
+                        <span className="text-sm font-medium text-neutral-500">
+                            BIAZA Photo Competition 2023
+                        </span>
+                    </span>
+                </Link>
             </div>
         </main>
     );

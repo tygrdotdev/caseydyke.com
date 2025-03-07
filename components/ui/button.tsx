2016c8a1a8963ed19627e57dc65a5fc63357104d
Ty Mason <hi@tygr.dev>
2025-03-07 17:09:26 +0000

    feat: upgrade to tailwind v4

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,18 +5,18 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-accent-950 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-accent-300",
+    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-accent-950 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-accent-300",
     {
         variants: {
             variant: {
                 default:
-                    "bg-accent-900 text-accent-50 shadow hover:bg-accent-900/90 dark:bg-accent-800 dark:text-accent-50 dark:hover:bg-accent-800/90",
+                    "bg-accent-900 text-accent-50 shadow-sm hover:bg-accent-900/90 dark:bg-accent-800 dark:text-accent-50 dark:hover:bg-accent-800/90",
                 destructive:
-                    "bg-red-500 text-neutral-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-neutral-50 dark:hover:bg-red-900/90",
+                    "bg-red-500 text-neutral-50 shadow-xs hover:bg-red-500/90 dark:bg-red-900 dark:text-neutral-50 dark:hover:bg-red-900/90",
                 outline:
-                    "border border-neutral-200 bg-white shadow-sm hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
+                    "border border-neutral-200 bg-white shadow-xs hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
                 secondary:
-                    "bg-neutral-300 text-neutral-900 shadow-sm hover:bg-neutral-300/80 dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-800/80",
+                    "bg-neutral-300 text-neutral-900 shadow-xs hover:bg-neutral-300/80 dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-800/80",
                 ghost: "hover:bg-neutral-300 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
                 link: "text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50",
             },
@@ -40,18 +40,27 @@ export interface ButtonProps
     asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-    ({ className, variant, size, asChild = false, ...props }, ref) => {
-        const Comp = asChild ? Slot : "button";
-        return (
-            <Comp
-                className={cn(buttonVariants({ variant, size, className }))}
-                ref={ref}
-                {...props}
-            />
-        );
+const Button = (
+    {
+        ref,
+        className,
+        variant,
+        size,
+        asChild = false,
+        ...props
+    }: ButtonProps & {
+        ref: React.RefObject<HTMLButtonElement>;
     }
-);
+) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+        <Comp
+            className={cn(buttonVariants({ variant, size, className }))}
+            ref={ref}
+            {...props}
+        />
+    );
+};
 Button.displayName = "Button";
 
 export { Button, buttonVariants };
